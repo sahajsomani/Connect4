932749cc091062deb21bba7c7b4d84c501477d6f
kimn1944 <45519304+kimn1944@users.noreply.github.com>
2019-02-03 18:22:55 -0500

    Update minimax.java

--- minimax.java
+++ minimax.java
@@ -1,3 +1,4 @@
+
 public class minimax extends classBot {
 
   public minimax(Game board) {
@@ -6,66 +7,95 @@ public class minimax extends classBot {
 
   @Override
   public int play() { //copying the game board matrix
-    int[][] mat = new int[board.getMatrix().length][board.getMatrix()[0].length];
+    int[][] state = new int[board.getMatrix().length][board.getMatrix()[0].length];
+    int player = board.getPlayer();
+    boolean[] actions = board.actions(state);
+    int move = -1;
+    
     for(int i = 0; i < board.getMatrix().length; i++) {
       for(int j = 0; j < board.getMatrix()[0].length; j++) {
-        mat[i][j] = board.getMatrix()[i][j];
-      }
-    }
-    return 0;
-  } //end play
-
-  private int minimaxDecision(int[][] state) {
-    int[] actions = new int[state[0].length];
-    int count = Integer.MIN_VALUE;
-    int action = -1; //initialised to -1 so that it throws an error if count does not change
-    int temp = count;
-    for(int i = 0; i < actions.length; i++) {
-      count = Math.max(count, minValue(board.result(state, i)));
-      if(count > temp) {
-        action = i;
-        temp = count;
-      }
-    }
-    return action;
-  } //end minimaxDecision
-
-  public int maxValue(int[][] state) {
-    if(board.terminalTest(state, board.getLen()) != 3) {
-      if(board.terminalTest(state, board.getLen()) == 2) {
-        return -1;
-      } else {
-        return board.terminalTest(state, board.getLen());
+        state[i][j] = board.getMatrix()[i][j];
       }
     }
     int v = Integer.MIN_VALUE;
-    boolean[] validActions = board.actions(state);
-    for(int i = 0; i < validActions.length; i++) {
-      if(board.validateInput(i,validActions)) {
-        v = Math.max(v,minValue(board.result(state, i)));
-      }
+    
+    for(int i = 0; i < actions.length; i++) {
+    	if(actions[i]) {
+    		int temp = minValue(board.result(state, i, player), player);
+    		if(temp > v) {
+    			v = temp;
+    			move = i;
+    		}
+    	}
     }
-    return v;
+    
+    return move;
+  } //end play
+
+  public int maxValue(int[][] state, int player) {
+	if(player == 1) {
+		player = 2;
+	} else {
+		player = 1;
+	}
+	
+	int util = board.terminalTest(state, board.getLen());
+	
+	if(util != 3) {
+	  if(util == 0) {
+		  return 0;
+	  } else {
+	    if(util == player) {
+	    	return 1;
+	    } else {
+	    	return -1;
+	    }
+	  }
+	}
+	int v = Integer.MIN_VALUE;
+	boolean[] actions = board.actions(state);
+	for(int i = 0; i < actions.length; i++) {
+	  if(actions[i]) {
+	    int temp = minValue(board.result(state, i, player), player);
+	    if(temp > v) {
+	    	v = temp;
+	    }
+	  }
+	}
+	return v;
   } //end maxValue
 
-  public int minValue(int[][] state) {
-    if(board.terminalTest(state, board.getLen()) != 3) {
-      if(board.terminalTest(state, board.getLen()) == 1) {
-        return -1;
-      } else if(board.terminalTest(state, board.getLen()) == 2){
-        return 1;
-      } else {
-        return board.terminalTest(state, board.getLen());
-      }
-    }
-    int v = Integer.MAX_VALUE;
-    boolean[] validActions = board.actions(state);
-    for(int i = 0; i < validActions.length; i++) {
-      if(board.validateInput(i,validActions)) {
-        v = Math.min(v,maxValue(board.result(state, i)));
-      }
-    }
-    return v;
+  public int minValue(int[][] state, int player) {
+  if(player == 1) {
+	  player = 2;
+  } else {
+	  player = 1;
+  }
+  
+  int util = board.terminalTest(state, board.getLen());
+  
+	if(util != 3) {
+	  if(util == 0) {
+		  return 0;
+	  } else {
+		  if(util == player) {
+			  return -1;
+		  } else {
+			  return 1;
+		  }
+	  }
+	}
+	int v = Integer.MAX_VALUE;
+	boolean[] actions = board.actions(state);
+	for(int i = 0; i < actions.length; i++) {
+	  if(actions[i]) {
+	    int temp = maxValue(board.result(state, i, player), player);
+	    if(temp < v) {
+	    	v = temp;
+	    }
+	  }
+	}
+	return v;
   } //end minValue
 
 } //end class
