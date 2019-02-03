e1c383a70d4093b29922da527d3ee22724da7a2c
kimn1944 <45519304+kimn1944@users.noreply.github.com>
2019-02-03 18:25:26 -0500

    Update randomPlayer.java

--- randomPlayer.java
+++ randomPlayer.java
@@ -1,3 +1,5 @@
+package connect4;
+
 import java.util.*;
 
 public class randomPlayer extends classBot{
@@ -9,9 +11,9 @@ public class randomPlayer extends classBot{
   @Override
   public int play() {
     Random rand = new Random();
-    int n = rand.nextInt(board[0].length) + 0;
-    while(board.validateCompInput(n) == false) {
-      n = rand.nextInt(board[0].length) + 0;
+    int n = rand.nextInt(board.getMatrix()[0].length) + 0;
+    while(board.validateInput(n, board.actions(board.getMatrix())) == false) {
+      n = rand.nextInt(board.getMatrix()[0].length) + 0;
     }
     return n;
   } //end play
