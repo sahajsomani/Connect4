acf381fd094e63083a02cbcb7f973845b783bdf5
kimn1944 <45519304+kimn1944@users.noreply.github.com>
2019-02-11 20:16:24 -0500

    Update minimax.java

--- minimax.java
+++ minimax.java
@@ -1,7 +1,6 @@
 package connect4;
 
 public class minimax extends classBot {
-	private long count = 0;
   public minimax(Game board) {
     super(board);
   }
@@ -9,7 +8,8 @@ public class minimax extends classBot {
   @Override
   public int play() { //copying the game board matrix
     int[][] state = new int[board.getMatrix().length][board.getMatrix()[0].length];
-
+    int move = -1;
+    
     for(int i = 0; i < board.getMatrix().length; i++) {
       for(int j = 0; j < board.getMatrix()[0].length; j++) {
         state[i][j] = board.getMatrix()[i][j];
@@ -17,75 +17,119 @@ public class minimax extends classBot {
     }
     boolean[] actions = board.actions(state);
     int player = board.getPlayer();
+    int depth = board.depth(state);
     
-    int move = -1;
-    int v = Integer.MIN_VALUE;
-    System.out.println(board.getLen());
+    counter = 0;
+    int[] v = {Integer.MIN_VALUE, depth};
     for(int i = 0; i < actions.length; i++) {
     	if(actions[i]) {
-    		int temp = minValue(board.result(state, i, player));
-    		if(temp >= v) {
+    		int[] temp = minValue(board.result(state, i, player));
+    		if(temp[0] > v[0]) {
     			v = temp;
     			move = i;
-    		}
+    		} else if(temp[0] == v[0]) {
+    	    	if(temp[0] <= 0) {
+    	    		if(temp[1] > v[1]) {
+    	    			v = temp;
+    	    			move = i;
+    	    		}
+    	    	} else {
+    	    		if(temp[1] < v[1]) {
+    	    			v = temp;
+    	    			move = i;
+    	    		}
+    	    	}
+    	    }
     	}
     }
     
     return move;
   } //end play
 
-  public int maxValue(int[][] state) {	
+  public int[] maxValue(int[][] state) {	
+	  counter++;
 	int util = board.terminalTest(state);
+	int depth = board.depth(state);
+	int[] result = {0, depth};
 	
 	if(util != 3) {
 	  if(util == 0) {
-		  return 0;
+		  return result;
 	  } else {
 	    if(util == board.getPlayer()) {
-	    	return 1;
+	    	result[0] = 1;
+	    	return result;
 	    } else {
-	    	return -1;
+	    	result[0] = -1;
+	    	return result;
 	    }
 	  }
 	}
-	int v = Integer.MIN_VALUE;
+	int[] v = {Integer.MIN_VALUE, depth};
 	boolean[] actions = board.actions(state);
 	for(int i = 0; i < actions.length; i++) {
 	  if(actions[i]) {
-	    int temp = minValue(board.result(state, i, board.getPlayer()));
-	    if(temp >= v) {
+	    int[] temp = minValue(board.result(state, i, board.getPlayer()));
+	    if(temp[0] > v[0]) {
 	    	v = temp;
+	    } else if(temp[0] == v[0]) {
+	    	if(temp[0] <= 0) {
+	    		if(temp[1] > v[1]) {
+	    			v = temp;
+	    		}
+	    	} else {
+	    		if(temp[1] < v[1]) {
+	    			v = temp;
+	    		}
+	    	}
 	    }
 	  }
 	}
-	return v;
+	result = v;
+	return result;
   } //end maxValue
 
-  public int minValue(int[][] state) {
+  public int[] minValue(int[][] state) {
+	  counter++;
 	  int util = board.terminalTest(state);
+	  int depth = board.depth(state);
+	  int[] result = {0, depth};
   
 	if(util != 3) {
 	  if(util == 0) {
-		  return 0;
+		  return result;
 	  } else {
 		  if(util == board.getPlayer()) {
-			  return 1;
+			  result[0] = 1;
+			  return result;
 		  } else {
-			  return -1;
+			  result[0] = -1;
+			  return result;
 		  }
 	  }
 	}
-	int v = Integer.MAX_VALUE;
+	int[] v = {Integer.MAX_VALUE, depth};
 	boolean[] actions = board.actions(state);
 	for(int i = 0; i < actions.length; i++) {
 	  if(actions[i]) {
-	    int temp = maxValue(board.result(state, i, flip(board.getPlayer())));
-	    if(temp <= v) {
+	    int[] temp = maxValue(board.result(state, i, flip(board.getPlayer())));
+	    if(temp[0] < v[0]) {
 	    	v = temp;
+	    } else if(temp[0] == v[0]) {
+	    	if(temp[0] <= 0) {
+	    		if(temp[1] < v[1]) {
+	    			v = temp;
+	    		}
+	    	} else {
+	    		if(temp[1] > v[1]) {
+	    			v = temp;
+	    		}
+	    	}
 	    }
 	  }
 	}
-	return v;
+	result = v;
+	return result;
   } //end minValue
 
   private int flip(int player) {
